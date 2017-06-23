511f65ee2e92ef37ecba38bde385c469cfb76f6a
KarolisRazmus <karolisrazmus@gmail.com>
2017-06-23 10:44:06 +0300

    2 commit Pay Pal works

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,10 @@ Route::get('/', function () {
 });
 
 
-Route::get('paypalform', ['as' => 'paypal.form', 'uses' => 'PaypalController@index']);
-Route::post('paypal/payment', ['as' => 'paypal.payment', 'uses' => 'PaypalController@payment']);
-Route::get('paypal/payment', ['as' => 'paypal.status', 'uses' => 'PaypalController@getPaymentStatus']);
+//Route::get('paypalform', ['as' => 'paypal.form', 'uses' => 'PaypalController@index']);
+//Route::post('paypal/payment', ['as' => 'paypal.payment', 'uses' => 'PaypalController@payment']);
+//Route::get('paypal/payment', ['as' => 'paypal.status', 'uses' => 'PaypalController@getPaymentStatus']);
+
+Route::get('paywithpaypal', array('as' => 'addmoney.paywithpaypal','uses' => 'AddMoneyController@payWithPaypal',));
+Route::post('paypal', array('as' => 'addmoney.paypal','uses' => 'AddMoneyController@postPaymentWithpaypal',));
+Route::get('paypal', array('as' => 'payment.status','uses' => 'AddMoneyController@getPaymentStatus',));
